40630796304e33d044e8fdcd76e907d6b2b4fb48
bongani-hub <94044452+bongani-hub@users.noreply.github.com>
2022-04-21 11:57:52 +0200

    react series5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Transition, animated } from "react-spring";
+import Component1 from "./Components/Component1";
+import Component2 from "./Components/Component2";
+import Component3 from "./Components/Component3";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  state = {
+    showComponent3: false
+  };
+
+  toggle = e => this.setState({ showComponent3: !this.state.showComponent3 });
+
+  render() {
+    return (
+      <div className="App">
+        <Component1 />
+        <Component2 toggle={this.toggle} />
+        <Transition
+          native
+          items={this.state.showComponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
